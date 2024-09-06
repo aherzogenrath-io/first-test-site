7f1ffa4624995ce6e21a1f824e928b45bed6268c
aherzogenrath-io <aherzogenrath@infonline.de>
2024-09-06 14:24:46 +0200

    function in external js file
    I added the function in the external java script file

--- feature-1.js
+++ feature-1.js
@@ -1,1 +1,6 @@
-function myFunction() {   }
+function turnOnLight() {
+    document.getElementById('myImage').src = 'pic_bulbon.gif';
+}
+function turnOffLight() {
+    document.getElementById('myImage').src = 'pic_bulboff.gif';
+}
